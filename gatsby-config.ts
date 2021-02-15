e7c6da9936a3a0a9ef120ea818b768656d88ec2c
Hamish Bray <hamish.bray@empired.com>
2021-02-15 11:21:39 +1100

    removed preview property in config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,11 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 export const siteMetadata = {
 	title: 'Gatsby Kentico Kontent POC',
 	description: 'Gatsby start site with Kentico Kontent source',
-	author: '@hamishbray'
+	author: '@hamishbray',
 }
 
 export const plugins = [
@@ -15,8 +15,8 @@ export const plugins = [
 		resolve: 'gatsby-source-filesystem',
 		options: {
 			name: 'images',
-			path: `${__dirname}/src/images`
-		}
+			path: `${__dirname}/src/images`,
+		},
 	},
 	'gatsby-transformer-sharp',
 	'gatsby-plugin-sharp',
@@ -29,21 +29,19 @@ export const plugins = [
 			background_color: '#663399',
 			theme_color: '#663399',
 			display: 'minimal-ui',
-			icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
-		}
+			icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+		},
 	},
 	{
 		resolve: '@kentico/gatsby-source-kontent',
 		options: {
 			projectId: process.env.KONTENT_PROJECT_ID,
-			usePreviewUrl: true,
-			authorizationKey: process.env.KONTENT_PREVIEW_API_KEY,
 			// Please note that with the Sample Project generated above, 'en-US' is the default language for the project and this config. For a blank project, this needs to be 'default'.
 			languageCodenames: [
-				'en-US' // Or the languages in your project (Project settings -> Localization)
-			]
-		}
-	}
+				'en-US', // Or the languages in your project (Project settings -> Localization)
+			],
+		},
+	},
 	// this (optional) plugin enables Progressive Web App + Offline functionality
 	// To learn more, visit: https://gatsby.dev/offline
 	// 'gatsby-plugin-offline',
