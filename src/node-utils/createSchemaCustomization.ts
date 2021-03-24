16cff3b7cc089c5baef874dbb42161f4de43686c
Hamish Bray <hamish.bray@empired.com>
2021-03-25 10:23:52 +1100

    cleanup

--- src/node-utils/createSchemaCustomization.ts
+++ src/node-utils/createSchemaCustomization.ts
@@ -1,71 +1,10 @@
-import {
-  GatsbyNode,
-  CreateSchemaCustomizationArgs,
-  NodePluginSchema,
-} from 'gatsby'
+import { GatsbyNode, CreateSchemaCustomizationArgs } from 'gatsby'
 
 export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] = ({
-  actions: { createFieldExtension, createTypes },
-  schema: { buildObjectType },
+	actions: { createTypes },
 }: CreateSchemaCustomizationArgs): any => {
-  // Create @url resolver for auto-generating url fields.
-  // createFieldExtension({
-  //   name: 'url',
-  //   args: {
-  //     slug: {
-  //       type: 'String!',
-  //       defaultValue: 'elements.url_pattern.value',
-  //     },
-  //   },
-  //   extend(options: any) {
-  //     return {
-  //       resolve(source: any) {
-  //         const slug = source[options.slug]
-  //         const type = source.internal.type
-  //         return resolveUrl(type, slug)
-  //       },
-  //     }
-  //   },
-  // })
-
-  // createFieldExtension({
-  //   name: 'slug',
-  //   args: {
-  //     slug: {
-  //       type: 'String!',
-  //       defaultValue: 'elements.slug',
-  //     },
-  //   },
-  //   extend(options: any) {
-  //     return {
-  //       resolve(source: any) {
-  //         return source[options.slug]
-  //       },
-  //     }
-  //   },
-  // })
-
-  // const slugs = buildObjectType({
-  // 	name: 'KontentItemArticle',
-  // 	interfaces: ['Node'],
-
-  // 	fields: {
-  // 		slug: {
-  // 			type: 'String!',
-  // 			resolve: (source, args, context, info) => {
-  // 				return source.elements.url_pattern.value
-  // 			},
-  // 		},
-  // 	},
-  // 	extensions: {
-  //		infer: true,
-  // 	},
-  // })
-
-  // createTypes([slugs])
-
-  // Create custom schema interfaces and extend types.
-  const typeDefs = `
+	// Create Searchable Item Node type
+	const typeDefs = `
     type SearchableItem implements Node @dontInfer {
       id: ID!
       content: String
@@ -81,25 +20,6 @@ export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] 
       url: String
     }
 	`
-  // interface NodeWithUrl implements Node {
-  //   id: ID!
-  //   url: String!
-  // }
-  // type KontentItemArticle implements Node @infer {
-  //   id: ID!
-  //   url: String!
-  // }
-  // type KontentItemCafe implements Node @infer {
-  //   id: ID!
-  //   url: String!
-  // }
-  // type KontentItemAccessory implements Node  @infer {
-  //   id: ID!
-  //   url: String!
-  // }
-  //`
 
-  createTypes(typeDefs)
+	createTypes(typeDefs)
 }
-
-const resolveUrl = (type: string, slug: string): string => `/${type}/${slug}`
--- src/node-utils/onCreateNode.ts
+++ src/node-utils/onCreateNode.ts
@@ -2,83 +2,105 @@
 import { GatsbyNode } from 'gatsby'
 import slugify from '@sindresorhus/slugify'
 import crypto from 'crypto'
+import { createRemoteFileNode } from 'gatsby-source-filesystem'
 
 export const onCreateNode: GatsbyNode['onCreateNode'] = ({
-  node,
-  actions: { createNodeField, createNode, createParentChildLink },
-  createNodeId,
+	node,
+	actions: { createNodeField, createNode, createParentChildLink },
+	createNodeId,
+	store,
+	cache,
 }) => {
-  const createSlugField = (value: string): void =>
-    createNodeField({
-      node,
-      name: `slug`,
-      value,
-    })
+	const createField = (name: string, value: string): void =>
+		createNodeField({
+			node,
+			name,
+			value,
+		})
 
-  const createSearchableItemNode = (typeFieldData: Object): void => {
-    const fieldData = {
-      modified: node.system.last_modified,
-      modified_unix: toUnix(node.system.last_modified),
-      type: node.system.type,
-      ...typeFieldData,
-    }
+	const createSearchableItemNode = (typeFieldData: Object): void => {
+		const fieldData = {
+			modified: node.system.last_modified,
+			modified_unix: toUnix(node.system.last_modified),
+			type: node.system.type,
+			...typeFieldData,
+		}
 
-    const nodeData = {
-      ...fieldData,
-      id: createNodeId(`${node.id}__SearchableItem`),
-      parent: node.id,
-      children: [],
-      internal: {
-        type: 'SearchableItem',
-        contentDigest: crypto
-          .createHash(`md5`)
-          .update(JSON.stringify(fieldData))
-          .digest(`hex`),
-      },
-    }
+		const nodeData = {
+			...fieldData,
+			id: createNodeId(`${node.id}__SearchableItem`),
+			parent: node.id,
+			children: [],
+			internal: {
+				type: 'SearchableItem',
+				contentDigest: crypto
+					.createHash(`md5`)
+					.update(JSON.stringify(fieldData))
+					.digest(`hex`),
+			},
+		}
 
-    createNode(nodeData)
-    createParentChildLink({ parent: node, child: nodeData })
-  }
+		createNode(nodeData)
+		createParentChildLink({ parent: node, child: nodeData })
+	}
 
-  let typeFieldData = {}
+	const createRemoteImageFileNode = async (): void => {
+		let fileNode
 
-  switch (node.internal.type) {
-    case 'kontent_item_accessory':
-      createSlugField(node.elements.url_pattern.value)
-      typeFieldData = {
-        content: node.elements.long_description.value,
-        summary: node.elements.short_description.value,
-        title: node.elements.product_name.value,
-        imageUrl: node.elements.image.value[0].url,
-        url: `/accessories/${node.fields.slug}`,
-      }
-      createSearchableItemNode(typeFieldData)
-      break
+		try {
+			fileNode = await createRemoteFileNode({
+				url: node.elements.image.value[0].url,
+				parentNodeId: node.id,
+				createNode,
+				createNodeId,
+				cache,
+				store,
+			})
+		} catch (error) {
+			console.log('Error creating remote file node: ', error)
+		}
 
-    case 'kontent_item_article':
-      createSlugField(node.elements.url_pattern.value)
-      typeFieldData = {
-        content: node.elements.body_copy.value,
-        summary: node.elements.summary.value,
-        title: node.elements.title.value,
-        published: node.elements.post_date.value,
-        published_unix: toUnix(node.elements.post_date.value),
-        imageUrl: node.elements.teaser_image.value[0].url,
-        url: `/articles/${node.fields.slug}`,
-      }
-      createSearchableItemNode(typeFieldData)
-      break
+		if (fileNode) node.featuredImg___NODE = fileNode.id
+	}
 
-    case 'kontent_item_cafe':
-      createSlugField(slugify(node.elements.city.value))
-      typeFieldData = {
-        title: node.elements.city.value,
-        imageUrl: node.elements.photo.value[0].url,
-        url: `/cafes/${node.fields.slug}`,
-      }
-      createSearchableItemNode(typeFieldData)
-  }
+	let typeFieldData = {}
+
+	switch (node.internal.type) {
+		case 'kontent_item_accessory':
+			createField('slug', node.elements.url_pattern.value)
+			typeFieldData = {
+				content: node.elements.long_description.value,
+				summary: node.elements.short_description.value,
+				title: node.elements.product_name.value,
+				imageUrl: node.elements.image.value[0].url,
+				url: `/accessories/${node.fields.slug}`,
+			}
+			createSearchableItemNode(typeFieldData)
+			break
+
+		case 'kontent_item_article':
+			createField('slug', node.elements.url_pattern.value)
+			typeFieldData = {
+				content: node.elements.body_copy.value,
+				summary: node.elements.summary.value,
+				title: node.elements.title.value,
+				published: node.elements.post_date.value,
+				published_unix: toUnix(node.elements.post_date.value),
+				imageUrl: node.elements.teaser_image.value[0].url,
+				url: `/articles/${node.fields.slug}`,
+			}
+			createSearchableItemNode(typeFieldData)
+			break
+
+		case 'kontent_item_cafe':
+			createField('slug', slugify(node.elements.city.value))
+			typeFieldData = {
+				title: node.elements.city.value,
+				imageUrl: node.elements.photo.value[0].url,
+				url: `/cafes/${node.fields.slug}`,
+			}
+			createSearchableItemNode(typeFieldData)
+	}
 }
 
 const toUnix = (date: string): string => Math.floor(new Date(date) / 1000)
