daf45febe4eabaa4135d010cc6f9e9aaf2b32447
Hamish Bray <hamish.bray@empired.com>
2021-05-12 11:01:34 +1000

    fixed up account routing

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -3,16 +3,16 @@ import { Router } from '@reach/router'
 
 import Layout from '../../components/layout'
 import PrivateRoute from '../../components/privateRoute'
-import Login from './login'
-import Profile from './profile'
+import Login from '../../components/login'
+import Profile from '../../components/profile'
 
 const Account: React.FC = () => (
-  <Layout>
-    <Router>
-      <PrivateRoute path="/account/profile" component={Profile} />
-      <Login path="/account/login" />
-    </Router>
-  </Layout>
+	<Layout>
+		<Router>
+			<PrivateRoute path="/account/profile" component={Profile} />
+			<Login path="/account/login" />
+		</Router>
+	</Layout>
 )
 
 export default Account
